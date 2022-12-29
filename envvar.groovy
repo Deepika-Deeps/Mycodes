56bd0345d022b387c02098fecbc6b98200c862a7
Deepika-Deeps <adeepikarani11@gmail.com>
2022-12-29 05:04:34 -0500

    smg

--- envvar.groovy
+++ envvar.groovy
@@ -1,11 +1,11 @@
 pipieline {
     agent any
     environment {
-        USER_NAME= "Deepika"
-        USER_ID= 644
+        USER_NAME = "Deepika"
+        USER_ID = 644
     } 
-    stages(Employee){
-        stage(Details){
+    stages{
+        stage(Details) {
             steps{
                 echo "employee Name is ${env.USER_NAME}"
                 echo "employee id is ${env.USER_ID}"
