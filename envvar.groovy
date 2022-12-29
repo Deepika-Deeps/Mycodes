ab7717ba4fd3af53723076d424f3dd8f98ddb499
Deepika-Deeps <adeepikarani11@gmail.com>
2022-12-29 04:28:45 -0500

    file

---
+++ envvar.groovy
@@ -0,0 +1,15 @@
+pipieline {
+    agent any
+    environment {
+        USER_NAME=Deepika
+        USER_ID= 644
+    } 
+    stages (Employee){
+        stage( Details){
+            steps{
+                echo "Name = ${env.USER_NAME}"
+                echo "id = ${env.USER_ID}"
+            }
+        }
+    }
+    
