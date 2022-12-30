1028443edc57669bfa6adfd9fba12918de0d16c3
Deepika-Deeps <adeepikarani11@gmail.com>
2022-12-30 00:52:27 -0500

    file

--- env2.sh
+++ env2.sh
@@ -1,10 +1,10 @@
 pipeline {
     agent any 
-    enivorment {
+    environment {
         USER_NAME = 'Deepika'
     }
     stages {
-        stages('Details') {
+        stage('Details') {
             steps {
                 sh "echo $USER_NAME"
                 sh "env |sort "
