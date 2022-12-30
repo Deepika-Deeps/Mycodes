59666c396780708187ae2a198e3d0709669c8ff4
Deepika-Deeps <adeepikarani11@gmail.com>
2022-12-30 02:07:46 -0500

    env3

--- env3.sh
+++ env3.sh
@@ -1,27 +1,10 @@
 pipeline {
     agent any 
-    parameters {
-  booleanParam description: 'is this is authorized employee', name: 'authorized'
-  choice choices: ['name ', 'id ', 'organisation'], description: 'select the details', name: 'details'
-  credentials credentialType: 'com.cloudbees.plugins.credentials.impl.UsernamePasswordCredentialsImpl', defaultValue: 'd5b846c5-ab03-4b14-81d7-554db2a8ee96', description: 'give the user name and password ', name: 'enter the values', required: false
-  string defaultValue: 'deepika', description: 'enter your name', name: 'name', trim: true
-  password defaultValue: 'deepika', description: 'enter the password', name: 'password'
-  run description: 'project is running', filter: 'ALL', name: 'run the project', projectName: 'env3 script'
-}
+    
     environment {
         COMPANY_NAME = 'infinite'
     }
     stages{
-        stage('hello'){
-            steps {
-                sh "echo authorized": ${params.'authorized'}
-                sh "echo details": ${params.'details'}
-                sh "echo enter the value":${params.'enter the values'}
-                sh "echo name":${params.name}
-                sh "echo password":${params.password}
-                sh "echo run th project":${params.'run the project'}
-            }
-        }
         stage('Employee1') {
             environment {
                 USER_NAME = 'deepika'
