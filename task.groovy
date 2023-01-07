11e99f05bc91ad1d609507e039ed8ed345fd13d9
Deepika-Deeps <adeepikarani11@gmail.com>
2023-01-07 07:41:06 -0500

    devops task file

---
+++ task.groovy
@@ -0,0 +1,19 @@
+pipeline {
+    agent any
+    stages {
+        stage('Git checkout stage')
+        {
+            steps {
+                git credrntialsId: 'github', ulr: 'https://github.com/valaxyTech/hello-worl.git'
+                echo "git checkout is successfull...."
+            }
+        }
+        stage('Maven Built stage')
+        {
+            steps {
+                mvn clean install
+                echo " Maven is installed...."
+            }
+        }
+    }
+}
